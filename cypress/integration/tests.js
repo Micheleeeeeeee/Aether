f420109f4f17bcb7082de93eed02b7e73232ae06
Silly Sock <sillysockcodes@gmail.com>
2021-10-02 14:09:07 +0930

    ?

--- cypress/integration/tests.js
+++
@@ -1,7 +0,0 @@
-describe('Test GET Request', () => {
-    it("Makes a get request on /", () => {
-        cy.request('http://localhost:3000')
-        cy.get('<pre>')
-        .should('contain', '{"failed":false,"message":"This works!","exitCode":0}');
-    })
-})
